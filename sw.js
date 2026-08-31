51b9406606c3494e3866fefb3ccbcc7d6327d242
edirosky <edirosky@users.noreply.github.com>
2026-08-31 21:47:11 -0100

    fonético: aceita letras com espaços (a b a d) ou coladas (abad), aliases Alfa/Xray, guarda X ray; SW v3

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    - Tiles OSM: stale-while-revalidate com limite LRU
    ============================================================ */
 
-const VERSION = "v2";
+const VERSION = "v3";
 const SHELL_CACHE = `scr-cipher-shell-${VERSION}`;
 const TILE_CACHE = `scr-cipher-tiles-${VERSION}`;
 const MAX_TILE_ENTRIES = 600;
