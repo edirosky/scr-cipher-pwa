c63e9af2568f0b198fe379dc1bf8cddaebccf737
edirosky <edirosky@users.noreply.github.com>
2026-08-31 23:23:51 -0100

    aba AJUDA com FAQ bilíngue (13 Q&A: acesso, KCV, KD, CHK, modos, fonético, offline, instalação, segurança, self-test); SW v7

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    - Tiles OSM: stale-while-revalidate com limite LRU
    ============================================================ */
 
-const VERSION = "v6";
+const VERSION = "v7";
 const SHELL_CACHE = `scr-cipher-shell-${VERSION}`;
 const TILE_CACHE = `scr-cipher-tiles-${VERSION}`;
 const MAX_TILE_ENTRIES = 600;
